25d57a50eda758cc1048c07ff0bb32e5f03196ce
Nicol Otárola <81721326+nico-op@users.noreply.github.com>
2021-08-29 22:56:00 -0600

    Versión final, comentado.

--- Cliente.java
+++ Cliente.java
@@ -7,59 +7,93 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.InputStreamReader;
-import java.net.ServerSocket;
 import java.net.Socket;
 import javax.swing.*;
 
 public class Cliente {
-    JFrame ventana_chat = null;
-    JButton boton_enviar = null;
-    JTextField txt_mensaje1 = null;
-    JTextField txt_mensaje2= null;
-    JTextField txt_mensaje3= null;
-    JTextArea area_chat = null;
-    JLabel precio= null;
-    JLabel impuesto = null;
-    JLabel peso = null;
-    JPanel contenedor_areachat = null;
-    JPanel contnedor_botontexto = null;
-    JPanel contenedor_labels = null;
-    JScrollPane scroll = null;
-    ServerSocket servidor = null;
-    Socket socket= null;
-    BufferedReader lector = null;
-    PrintWriter escritor= null;
+    /**
+     * Cliente
+     * Contiene la clase Cliente, cuenta con interfaz y cálculos
+     * @Autor: Nicol Otárola Porras
+     * @Versión: 29/08/2021
+     */
 
+    // Declaración de variables
+    JFrame ventana_chat;
+    JButton boton_enviar;
 
-    public Cliente(){
+    // campos de texto para ingresar datos
+    JTextField txt_mensaje1;
+    JTextField txt_mensaje2;
+    JTextField txt_mensaje3;
+    JTextArea area_chat;
+
+    // labels
+    JLabel precio;
+    JLabel impuesto;
+    JLabel peso;
+
+    // contenedores
+    JPanel contenedor_areachat;
+    JPanel contnedor_botontexto;
+    JPanel contenedor_labels;
+
+    JScrollPane scroll; // scroll
+
+    Socket socket;// socket
+
+    // escritor y lector
+    BufferedReader lector;
+    PrintWriter escritor;
+
+    /**
+     * método para generar la interfaz del cliente
+     */
+    public Cliente() {
         hacerInterfaz();
 
     }
-    public  void hacerInterfaz(){
-        ventana_chat = new JFrame("Cliente");
-        boton_enviar= new JButton("Enviar");
+
+    /**
+     * método para construir la interfaz del cliente
+     */
+    public void hacerInterfaz() {
+
+        // Toda la distrubición de los elementos que contiene la interfaz
+
+        ventana_chat = new JFrame("Cliente");// ventana principal de la interfaz
+        boton_enviar = new JButton("Enviar");// botón para enviar los datos ingresados en los campos de texto
+
+        // campos de texto para los datos
         txt_mensaje1 = new JTextField(4);
         txt_mensaje2 = new JTextField(5);
         txt_mensaje3 = new JTextField(6);
-        area_chat= new JTextArea(20,12);
+        area_chat = new JTextArea(20, 12);
+
+        //labels
         precio = new JLabel("Precio ");
         impuesto = new JLabel("Impuesto");
         peso = new JLabel("Peso");
-        scroll = new JScrollPane(area_chat);
+
+        scroll = new JScrollPane(area_chat);//scroll
+
+        // contenedores y tamaños
         contenedor_areachat = new JPanel();
-        contenedor_areachat.setLayout(new GridLayout(1,1));
+        contenedor_areachat.setLayout(new GridLayout(1, 1));
         contenedor_areachat.add(scroll);
         contenedor_labels = new JPanel();
-        contenedor_labels.setLayout(new GridLayout(1,1));
+        contenedor_labels.setLayout(new GridLayout(1, 1));
         contenedor_labels.add(precio);
         contenedor_labels.add(impuesto);
         contenedor_labels.add(peso);
         contnedor_botontexto = new JPanel();
-        contnedor_botontexto.setLayout(new GridLayout(1,4));
+        contnedor_botontexto.setLayout(new GridLayout(1, 4));
         contnedor_botontexto.add(txt_mensaje1);
         contnedor_botontexto.add(txt_mensaje2);
         contnedor_botontexto.add(txt_mensaje3);
         contnedor_botontexto.add(boton_enviar);
+
+        // características de la ventana principal y ubicciones de los contenedores
         ventana_chat.setLayout(new BorderLayout());
         ventana_chat.add(contenedor_labels, BorderLayout.CENTER);
         ventana_chat.add(contenedor_areachat, BorderLayout.NORTH);
@@ -71,30 +105,40 @@ public class Cliente {
 
         Thread principal = new Thread(new Runnable() {
             @Override
+            /**
+             * método para establecer el puerto de conexión que va a utilizar el cliente
+             * con el servidor, y mantener activos los métodos de escritura y lectura.
+             *@throws Exception
+             */
             public void run() {
-                try{
-                socket = new Socket("Localhost", 9000);
-                leer();
-                escribir();
+                try {
+                    socket = new Socket("Localhost", 9000);
+                    leer();
+                    escribir();
                 } catch (Exception ex) {
                     ex.printStackTrace();
                 }
             }
         });
-        principal.start();
-
+        principal.start(); // se inicializa el hilo principal para el socket
     }
-
-    public void leer(){
+    /**
+     * método crear un hilo del lector de datos
+     */
+    public void leer() {
         Thread leer_hilo = new Thread(new Runnable() {
             @Override
+            /**
+             *método para leer los datos ingresados en los espacios de texto
+             * @throws Exception
+             */
             public void run() {
                 try {
-                    lector= new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                        while (true){
-                            String mensaje_recibido = lector.readLine();
-                            area_chat.append("Servidor dice: " + mensaje_recibido +"\n");
-                        }
+                    lector = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                    while (true) {
+                        String mensaje_recibido = lector.readLine();
+                        area_chat.append("Servidor dice: " + mensaje_recibido + "\n");
+                    }
                 } catch (Exception ex) {
                     ex.printStackTrace();
                 }
@@ -102,14 +146,24 @@ public class Cliente {
         });
         leer_hilo.start();
     }
+    /**
+     * método para crear el hilo del escritor de datos
+     */
     public void escribir() {
         Thread escribir_hilo = new Thread(new Runnable() {
             @Override
+            /**
+             * método para crear el escritor de los datos ingresados en los espacios de texto
+             */
             public void run() {
                 try {
                     escritor = new PrintWriter(socket.getOutputStream(), true);
                     boton_enviar.addActionListener(new ActionListener() {
                         @Override
+                        /**
+                         * método para escribir los datos ingresados en los espacios de texto
+                         * @throws Exception
+                         */
                         public void actionPerformed(ActionEvent e) {
                             String enviar_mensaje1 = txt_mensaje1.getText();
                             String enviar_mensaje2 = txt_mensaje2.getText();
@@ -120,13 +174,12 @@ public class Cliente {
                             escritor.println(precio);
                             escritor.println(impuesto);
                             escritor.println(peso);
+                            escritor.println(calculo(precio,impuesto,peso));
                             txt_mensaje1.setText("");
                             txt_mensaje2.setText("");
                             txt_mensaje3.setText("");
-
                         }
                     });
-
                 } catch (Exception ex) {
                     ex.printStackTrace();
                 }
@@ -135,16 +188,23 @@ public class Cliente {
         escribir_hilo.start();
     }
 
+    /**
+     * método para calcular el resultado final de la operación en función de sus parametros
+     * @param precio
+     * @param impuesto
+     * @param peso
+     * @return
+     * resiltaod final del cálculo
+     */
     public double calculo(int precio, int impuesto, int peso) {
         double total = (precio * impuesto / 100) + (peso * 0.15d);
         return total;
     }
-    public static void main(String[] args){
+    /**
+     * método para inicializar un nuevo servidor
+     * @param args
+     */
+    public static void main (String[]args){
         new Cliente();
-
     }
-
-
-
-
 }
--- Servidor.java
+++ Servidor.java
@@ -6,40 +6,80 @@ import java.net.*;
 import java.io.*;
 import java.awt.event.*;
 
+/**
+ * Servidor:
+ * Esta es la clase del servidor, contien una interfaz
+ * funcionalidad de calculo y el factor de coonexión.
+ * @Autor: Nicol Otárola Porras
+ * @Versión: 29/08/2021
+ */
 public class Servidor {
-    JFrame ventana_chat = null;
-    JButton btn_enviar = null;
-    JTextField txt_mensaje1= null;
-    JTextField txt_mensaje2= null;
-    JTextField txt_mensaje3= null;
-    JTextArea area_chat= null;
-    JPanel contenedorChat= null;
-    JPanel contenerdorBtntxt= null;
-    JScrollPane scroll= null;
-    ServerSocket servidor = null;
-    Socket socket= null;
-    BufferedReader lector = null;
-    PrintWriter escritor= null;
-    JLabel precio= null;
-    JLabel impuesto = null;
-    JLabel peso = null;
-    JPanel contenedor_labels = null;
 
+    // declaración de variables
+
+    JFrame ventana_chat; // ventana principal
+    JButton btn_enviar; // botón para envier los datos
+
+    // campos de texto para ingresar los datos
+
+    JTextField txt_mensaje1;
+    JTextField txt_mensaje2;
+    JTextField txt_mensaje3;
+
+    JTextArea area_chat; // area de texto para mostrar los dato
+
+    // contenedores
+    JPanel contenedorChat;
+    JPanel contenerdorBtntxt;
+
+    JScrollPane scroll; // scroll
+
+    // Sockets
+    ServerSocket servidor;
+    Socket socket;
+
+    // Lector y escritor
+    BufferedReader lector;
+    PrintWriter escritor;
+
+    // Labels
+    JLabel precio;
+    JLabel impuesto;
+    JLabel peso;
+    JPanel contenedor_labels;
+
+
+    /**
+     * método para generar la interfaz del servidor
+     */
     public Servidor(){
         hacerInterfaz();
     }
 
+    /**
+     * Método para construir la interfaz del servidor
+     */
     public void hacerInterfaz(){
-        ventana_chat = new JFrame("Servidor");
-        btn_enviar = new JButton("Enviar");
+
+        // Toda la distribución de los elementos que contiene la interfaz
+
+        ventana_chat = new JFrame("Servidor"); // ventana para el servidor
+        btn_enviar = new JButton("Enviar");// botón para enviar los datos
+
+        // campos de texto para los datos
         txt_mensaje1 = new JTextField(4);
         txt_mensaje2 = new JTextField(5);
         txt_mensaje3 = new JTextField(6);
         area_chat= new JTextArea(20,12);
-        scroll = new JScrollPane(area_chat);
+
+        scroll = new JScrollPane(area_chat); // scroll
+
+        // labels
         precio = new JLabel("Precio ");
         impuesto = new JLabel("Impuesto");
         peso = new JLabel("Peso");
+
+        // contenedores y tamaños
         contenedor_labels = new JPanel();
         contenedor_labels.setLayout(new GridLayout(1,1));
         contenedor_labels.add(precio);
@@ -54,6 +94,8 @@ public class Servidor {
         contenerdorBtntxt.add(txt_mensaje2);
         contenerdorBtntxt.add(txt_mensaje3);
         contenerdorBtntxt.add(btn_enviar);
+
+        // características de la ventana principal y ubicciones de los contenedores
         ventana_chat.setLayout(new BorderLayout());
         ventana_chat.add(contenedor_labels, BorderLayout.CENTER);
         ventana_chat.add(contenedorChat, BorderLayout.NORTH);
@@ -63,11 +105,17 @@ public class Servidor {
         ventana_chat.setResizable(false);
         ventana_chat.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        // hilo principal para el socket
         Thread principal = new Thread(new Runnable() {
             @Override
+            /**
+             * método para establecer el puerto de conexión que va a utilizar el servidor
+             * con los cliente, y mantener activos los métodos de escritura y lectura.
+             * @throws IOException
+             */
             public void run() {
                 try{
-                servidor = new ServerSocket(9000);
+                    servidor = new ServerSocket(9000);
 
                     while (true){
                         socket = servidor.accept();
@@ -81,49 +129,67 @@ public class Servidor {
 
             }
         });
-        principal.start();
+        principal.start(); // se inicia el hilo principal para el socket
 
     }
 
+    /**
+     * método crear un hilo del lector de datos
+     */
     public void leer(){
         Thread leer_hilo = new Thread(new Runnable() {
             @Override
+            /**
+             * método para leer los datos ingresados en los espacios de texto
+             * @throws IOException
+             */
             public void run() {
                 try {
                     lector= new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                        while (true){
-                            String mensaje_recibido = lector.readLine();
-                            area_chat.append("Cliente dice: " + mensaje_recibido +"\n");
+                    while (true){
+                        String mensaje_recibido = lector.readLine();
+                        area_chat.append("Cliente dice: " + mensaje_recibido +"\n");
                     }
                 } catch (Exception ex) {
                     ex.printStackTrace();
                 }
             }
         });
-        leer_hilo.start();
+        leer_hilo.start(); // se inicia el hilo para leer los datos
     }
-    public void escribir() {
+
+    /**
+     * método para crear el hilo del escritor de datos
+     */
+    public void escribir() { // método para escribir los datos
         Thread escribir_hilo = new Thread(new Runnable() {
             @Override
+            /**
+             * método para crear el escritor de los datos ingresados en los espacios de texto
+             */
             public void run() {
                 try {
-                escritor = new PrintWriter(socket.getOutputStream(), true);
-                btn_enviar.addActionListener(new ActionListener() {
-                     @Override
-                     public void actionPerformed(ActionEvent e) {
-                         String enviar_mensaje1 = txt_mensaje1.getText();
-                         String enviar_mensaje2 = txt_mensaje2.getText();
-                         String enviar_mensaje3 = txt_mensaje3.getText();
-                         int precio = Integer.parseInt(enviar_mensaje1);
-                         int impuesto = Integer.parseInt(enviar_mensaje2);
-                         int peso = Integer.parseInt(enviar_mensaje3);
-                         escritor.println(precio);
-                         escritor.println(impuesto);
-                         escritor.println(peso);
-                         escritor.println(calculo(precio,impuesto,peso));
-                         txt_mensaje1.setText("");
-                         txt_mensaje2.setText("");
-                         txt_mensaje3.setText("");
+                    escritor = new PrintWriter(socket.getOutputStream(), true);
+                    btn_enviar.addActionListener(new ActionListener() {
+                        @Override
+                        /**
+                         * método para escribir los datos ingresados en los espacios de texto
+                         * @throws IOException
+                         */
+                        public void actionPerformed(ActionEvent e) {
+                            String enviar_mensaje1 = txt_mensaje1.getText();
+                            String enviar_mensaje2 = txt_mensaje2.getText();
+                            String enviar_mensaje3 = txt_mensaje3.getText();
+                            int precio = Integer.parseInt(enviar_mensaje1);
+                            int impuesto = Integer.parseInt(enviar_mensaje2);
+                            int peso = Integer.parseInt(enviar_mensaje3);
+                            escritor.println(precio);
+                            escritor.println(impuesto);
+                            escritor.println(peso);
+                            escritor.println(calculo(precio,impuesto,peso));
+                            txt_mensaje1.setText("");
+                            txt_mensaje2.setText("");
+                            txt_mensaje3.setText("");
 
                         }
                     });
@@ -133,16 +199,27 @@ public class Servidor {
                 }
             }
         });
-        escribir_hilo.start();
-
-
-    }
-        public double calculo(int precio, int impuesto, int peso) {
-            double total = (precio * impuesto / 100) + (peso * 0.15d);
-            return total;
-        }
-        public static void main (String[]args){
-            new Servidor();
-        }
+        escribir_hilo.start(); // se inicia el hilo para escribir los datos
     }
 
+    /**
+     * método para calcular el resultado final de la operación en función de sus parametros
+     * @param precio
+     * @param impuesto
+     * @param peso
+     * @return
+     * Resultado final del cálculo
+     */
+    public double calculo(int precio, int impuesto, int peso) {
+        double total = (precio * impuesto / 100) + (peso * 0.15d);
+        return total;
+    }
+
+    /**
+     * método para inicializar un nuevo servidor
+     * @param args
+     */
+    public static void main (String[]args){
+        new Servidor();
+    }
+}
